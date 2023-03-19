8bc38f7d0b1b94f2220c0da2e5d28c2436a3ad31
hyzhao <1048997225@qq.com>
2023-03-19 21:51:34 +0800

    feat: 添加数据库 & 增删改查

---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+class Post {
+  // 主键是用于在表中唯一标识行的列。虽然我们可以使用现有的列并将其设置为主键，但通常会创建一个id列。通过选择TypeORM中的PrimaryGeneratedColumn，我们创建了一个自动生成值的整数主键列。
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public content: string;
+}
+
+export default Post;
